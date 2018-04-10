0218dd9851e2b68dc6751dcc7b5bbaefffe88466
RPC11\Karthikeyan <karthikarwinverma@gmail.com>
2018-04-10 16:25:24 +0530

    Password Match Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 function usernameCheck(c: AbstractControl): {[key: string]: Boolean} | null {
   if (c.value === 'karthik') {
     return { 'name' : true };
+  }
+  return null;
+}
+function ageCheck(min, max): ValidatorFn {
+  return (c: AbstractControl): {[key: string]: Boolean} | null => {
+    if (c.value !== undefined && (isNaN(c.value) || c.value < min || c.value > max)) {
+      return { 'range' : true };
+    }
+    return null;
+  };
+}
+function passwordCheck(c: AbstractControl): {[key: string]: Boolean} | null {
+  let passVal = c.get('password');
+  let cPassVal = c.get('cpassword');
+  if (passVal.pristine || cPassVal.pristine) {
+    return null;
+  }
+  if (passVal.value !== cPassVal.value) {
+    return { 'match' : true };
   }
   return null;
 }
@@ -25,7 +44,11 @@ export class AppComponent implements OnInit {
       username: ['', [Validators.required, Validators.minLength(3), usernameCheck]],
       // tslint:disable-next-line:max-line-length
       email: [{'value' : '', 'disabled': false}, [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      passwords: this.fb.group({
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        cpassword: ['', [Validators.required]]
+      }, {validator: passwordCheck}),
+      age: [0, [Validators.required, ageCheck(13, 19)]]
     });
     /* this.userForm = new FormGroup({
       username: new FormControl(),
